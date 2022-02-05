33dfece9e7ad7f0824a9637fac06137928056428
sanmiguelu <usanmiguelalzorriz@nordex-online.com>
2022-02-05 16:44:27 +0100

    Add tournament matches

--- src/competitions/main.py
+++ src/competitions/main.py
@@ -1,5 +1,4 @@
-def add_one(number):
-    return number + 1
+import itertools as it
 
 class Match(object):
     
@@ -12,8 +11,24 @@ class Match(object):
         self.ot = ot
         self.finished = False
         if self.result is not None:
-            self.finished = True
-    
+            self.set_result(result, ot)
+
+    def set_result(self, result, ot=False):
+        self.result = result
+        self.finished = True
+        
+        # TODO: goals and points in this match different than in the tournament
+        # Goals
+        self.home_team.goals_scored += result[0]
+        self.home_team.goals_received += result[1]
+        self.away_team.goals_scored += result[1]
+        self.away_team.goals_received += result[0]
+        
+        winner = result[1] - result[0]
+        # Points
+        self.home_team
+
+
 class Team(object):
     
     def __init__(self, team_name) -> None:
@@ -23,4 +38,16 @@ class Team(object):
         self.goals_scored = 0
         self.goals_received = 0
         self.points = 0
+        
+    
+    def __repr__(self) -> str:
+        return f"{type(self)} {self.name} at {hex(id(self))}"
+
+class Tournament(object):
+
+    def __init__(self, list_of_teams=None, rounds=2) -> None:
+
+        self.teams = [Team(t) for t in list_of_teams]
+
+        self.matches = [Match(a, b) for a, b in it.permutations(list_of_teams, 2)]
         pass
